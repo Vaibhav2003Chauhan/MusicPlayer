cc4ae7ff4b29a718fc87af222611fdb23a0cb8b1
vaibhav chauhan <vishuchauhan461@gmail.com>
2023-12-02 15:35:09 +0530

    completed Styling

---
+++ script.js
@@ -0,0 +1,26 @@
+// const music = ["Pata Nahi Kis Roop Mein Aakar_320(PagalWorld)","Taaron Ke Shehar Mein(PagalWorld)"]
+// var i=0;
+
+// function next()
+// { 
+//     console.log("Next music")
+//     let musicname = document.getElementById("nextmusic")
+   
+//     if(i==0)
+//     {
+//         musicname.src = "../assests/Music"+music[i]+".mp3"
+//         console.log(musicname)
+       
+//     }
+//     let nextmus=music[i+1]
+//     console.log(nextmus)
+//     musicname=`../assets/Music/${nextmus}.mp3`
+// }
+
+async function api()
+{
+
+
+}
+
+api();
