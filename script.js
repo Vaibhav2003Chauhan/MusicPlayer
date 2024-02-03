a5d0ffa01cfe3c12ccdc7d206830882da3dd0f7f
vaibhav chauhan <vishuchauhan461@gmail.com>
2024-02-04 03:09:34 +0530

    All functionalities added

--- script.js
+++ script.js
@@ -1,57 +1,63 @@
-// function to play the Music 
-let songs = [
-    {
-        artist: 'hbdajholgfUAJDEF',
-        songname: 'Pata Nahi Kis Roop'
-    },
+var playmusic = document.getElementById("playmusic")
+let playing_music = false // music is stooped initially
+var music = document.querySelector("audio")
+var image = document.querySelector('img')
+var nextmusic = document.getElementById('next')
+var previousmusic = document.getElementById("back")
+let songname = document.querySelector('h3')
+let artist = document.getElementsByClassName("artist")
+var songnumber = -1
+songslist = [{
+    songname: 'TarrokaSeher',
+    artist: 'Arijit Singh'
+},
+{
+    songname: '295',
+    artist: 'Sidhu Mosewala'
+}, {
+    songname: 'BrownRang',
+    artist: 'Honey Singh'
+}, {
+    songname: 'AngreziBeat',
+    artist: 'POP'
+}]
 
-    {
+playmusic.addEventListener("click", () => {
+    if (playing_music == false) {
+        playing_music = true
+        playmusic.classList.replace("fa-play", "fa-pause")
+        image.classList.add('rotate')
+        music.play()
 
-        artist: 'NULL',
-        songname: 'TarrokaSeher'
-    }
-]
-var play = document.getElementById("playmusic")
-var is_pause = false // no pause
-var next = document.getElementById('nextmusic')
-var prev=document.getElementById('previous')
-let musicname = document.querySelector('h3');
-let artistname=document.getElementsByClassName("artist");
-var songindex=0
-// console.log("nodd")
-
-
-var music = document.querySelector('audio')
-
-
-play.addEventListener("click", () => {
-        if (is_pause == false) {
-        is_pause = true
-        play.classList.replace("fa-play", "fa-pause")
-        console.log("pause")
-        music.play();
+        console.log("played the music ")
     }
     else {
-        is_pause = false
-        play.classList.replace("fa-pause", "fa-play")
-        console.log("Play")
+        playing_music = false
+        playmusic.classList.replace("fa-pause", "fa-play")
+        image.classList.remove('rotate')
         music.pause()
-
+        console.log("pause the music ")
     }
-
     console.log(music)
 })
 
-function loadmusic(songs)
-{
-  musicname.innerHTML=songs[songindex].songname;
-  artistname[0].innerHTML = songs[songindex].artist
-  
+function loadmusic(songslist) {
+    music.src = "../assets/Music/" + songslist.songname + ".mp3"
+    console.log(music.src)
+    songname.textContent = songslist.songname
+    artist[0].innerHTML = songslist.artist
+    music.play()
 }
-loadmusic(songs)
 
-next.addEventListener('click', () => {
-    is_pause = true
+nextmusic.addEventListener("click", () => {
+    songnumber = (songnumber + 1) % songslist.length;
+    loadmusic(songslist[songnumber])
+}
+)
+previousmusic.addEventListener("click", () => {
 
+    songnumber = (songnumber - 1 + songslist.length) % songslist.length;
 
-})
+    loadmusic(songslist[songnumber])
+}
+)
